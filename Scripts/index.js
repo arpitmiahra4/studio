10877ef8faf2d746cd8d661a04436dfcfcf96572
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-04-07 10:54:38 +0530

    adding: eventListener to every color button

--- Scripts/index.js
+++ Scripts/index.js
@@ -7,3 +7,7 @@ let buton = document.querySelector(".upload_logo");
 buton.style.backgroundColor = "#34b5e5"
 
 let img = document.querySelector(".logo_here");
+
+let blue = document.querySelector(".blue_color").addEventListener("click", changeToBlue);
+let pink = document.querySelector(".pink_color").addEventListener("click", changeToPink);
+let yellow = document.querySelector(".yellow_color").addEventListener("click", changeToYellow);
