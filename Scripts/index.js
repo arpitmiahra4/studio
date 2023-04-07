52beeb165107f9bccc2070533488ec1195cf5c70
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-04-07 10:55:23 +0530

    adding: change umbrella and as well as bgColor function

--- Scripts/index.js
+++ Scripts/index.js
@@ -11,3 +11,22 @@ let img = document.querySelector(".logo_here");
 let blue = document.querySelector(".blue_color").addEventListener("click", changeToBlue);
 let pink = document.querySelector(".pink_color").addEventListener("click", changeToPink);
 let yellow = document.querySelector(".yellow_color").addEventListener("click", changeToYellow);
+
+function changeToBlue() {
+    display_image.src = "../images/Blue umbrella.png"
+    main_box.style.backgroundColor = "#e5f5fe"
+    buton.style.backgroundColor = "#34b5e5"
+    img.src = "../images/logo.png"
+}
+function changeToPink() {
+    display_image.src = "../images/Pink umbrella.png";
+    main_box.style.backgroundColor = "#F9EBF9"
+    buton.style.backgroundColor = "#d9378d"
+    img.src = "../images/logo.png"
+}
+function changeToYellow() {
+    display_image.src = "../images/Yello umbrella.png"
+    main_box.style.backgroundColor = "#fffaed"
+    buton.style.backgroundColor = "#fccf41"
+    img.src = "../images/logo.png"
+}
