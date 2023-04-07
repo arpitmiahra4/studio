e9cdd5fb9e1d86d678903d41c11db1c5c4b4b846
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-04-07 10:53:48 +0530

    called: every element needed in js

---
+++ Scripts/index.js
@@ -0,0 +1,9 @@
+// put JavaScript here...
+let main_box = document.querySelector("#main_box");
+main_box.style.backgroundColor = "#e6f6fc";
+let display_image = document.querySelector(".umbrella_src")
+display_image.src = "../images/Blue umbrella.png"
+let buton = document.querySelector(".upload_logo");
+buton.style.backgroundColor = "#34b5e5"
+
+let img = document.querySelector(".logo_here");
