0890e9611af998b1e71dfaee51f80dcd988cfa0f
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-04-07 19:30:14 +0530

    adding: animations timeout and logo upload function

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,32 +1,95 @@
 // put JavaScript here...
+let changeObj = {
+    blue_color: {
+        umbrellaSrc: "../images/Blue umbrella.png",
+        mainBoxBgColor: "#e5f5fe",
+        btnBgColor: "#34b5e5",
+        imgSrc: ""
+    },
+    pink_color: {
+        umbrellaSrc: "../images/Pink umbrella.png",
+        mainBoxBgColor: "#F9EBF9",
+        btnBgColor: "#d9378d",
+        imgSrc: ""
+    },
+    yellow_color: {
+        umbrellaSrc: "../images/Yello umbrella.png",
+        mainBoxBgColor: "#fffaed",
+        btnBgColor: "#fccf41",
+        imgSrc: ""
+    },
+}
+let img = document.querySelector(".logo_here");
+let blue = document.querySelector(".blue_color")
+let pink = document.querySelector(".pink_color")
+let yellow = document.querySelector(".yellow_color")
 let main_box = document.querySelector("#main_box");
 main_box.style.backgroundColor = "#e6f6fc";
+
+let button_div = document.querySelector(".color_selector").addEventListener("click", colorChangerFunction)
+function colorChangerFunction(e) {
+    console.log(e.target.className);
+    let color = e.target.className;
+    themeChanger(changeObj[color])
+}
 let display_image = document.querySelector(".umbrella_src")
 display_image.src = "../images/Blue umbrella.png"
 let buton = document.querySelector(".upload_logo");
-buton.style.backgroundColor = "#34b5e5"
+let logoImage = undefined;
+let logoIsUploading = false;
+const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+            resolve(fileReader.result);
+        };
+        fileReader.onerror = () => {
+            reject(fileReader.error);
+        };
+    });
+};
+buton.addEventListener("change", (e) => {
+    let file = e.target.files[0];
+    logoIsUploading = true;
+    display_image.style.display = "none";
+    img.setAttribute("src", "../images/loader_icon.svg");
+    img.setAttribute('class', 'loader_image');
+    setTimeout(async () => {
+        try {
+            let base64File = await convertBase64(file);
+            logoImage = base64File.toString();
+            logoIsUploading = false;
+            if (!logoIsUploading && logoImage) {
+                display_image.style.display = "block"
+                img.setAttribute("src", logoImage);
+                
+                img.setAttribute('class', '');
+            }
+        } catch (err) {
+            alert(err.message);
+        }
+    }, 2000);
+})
 
-let img = document.querySelector(".logo_here");
 
-let blue = document.querySelector(".blue_color").addEventListener("click", changeToBlue);
-let pink = document.querySelector(".pink_color").addEventListener("click", changeToPink);
-let yellow = document.querySelector(".yellow_color").addEventListener("click", changeToYellow);
+function themeChanger(payload) {
+    const { umbrellaSrc, mainBoxBgColor, btnBgColor, imgSrc } = payload;
+    logoIsUploading = true;
+    display_image.style.display = "none";
+    img.setAttribute("src", "../images/loader_icon.svg");
+    img.setAttribute('class', 'loader_image');
+    setTimeout(() => {
+        display_image.src = umbrellaSrc
+        main_box.style.backgroundColor = mainBoxBgColor
+        buton.style.backgroundColor = btnBgColor
+        img.src = imgSrc
 
-function changeToBlue() {
-    display_image.src = "../images/Blue umbrella.png"
-    main_box.style.backgroundColor = "#e5f5fe"
-    buton.style.backgroundColor = "#34b5e5"
-    img.src = "../images/logo.png"
-}
-function changeToPink() {
-    display_image.src = "../images/Pink umbrella.png";
-    main_box.style.backgroundColor = "#F9EBF9"
-    buton.style.backgroundColor = "#d9378d"
-    img.src = "../images/logo.png"
-}
-function changeToYellow() {
-    display_image.src = "../images/Yello umbrella.png"
-    main_box.style.backgroundColor = "#fffaed"
-    buton.style.backgroundColor = "#fccf41"
-    img.src = "../images/logo.png"
+        logoIsUploading = false;
+        display_image.style.display = "block"
+        if (!logoIsUploading && logoImage) {
+            img.setAttribute("src", logoImage);
+            img.setAttribute('class', '');
+        }
+    }, 3000)
 }
